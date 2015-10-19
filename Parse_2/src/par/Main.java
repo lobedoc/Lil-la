432767f7354d8032b1f6ee432331014cf971b91d
administrator <administrator@192.168.1.101>
2015-10-19 22:49:50 +0200

    asd

--- Parse_2/src/par/Main.java
+++ Parse_2/src/par/Main.java
@@ -8,7 +8,7 @@ import javax.swing.UnsupportedLookAndFeelException;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stubb
 		 try {
 			UIManager.setLookAndFeel(
 			            UIManager.getSystemLookAndFeelClassName());
